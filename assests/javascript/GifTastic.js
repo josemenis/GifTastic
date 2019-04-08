ff428667788ea7d23d8c8018fe8c399df108a932
Little Milo <no-reply>
2019-04-07 18:58:36 -0700

    a little closer to the finishline

--- assests/javascript/GifTastic.js
+++ assests/javascript/GifTastic.js
@@ -1,48 +1,50 @@
+// Adding click event listen listener to all buttons
+document.querySelector("#container").addEventListener("click", function(event) {
+
+  if (event.target.tagName == "BUTTON") {
+  // Grabbing and storing the data-animal property value from the button
+  var animal = event.target.dataset.animal;
+ 
+ 
  // queryURL for Giphy API
- var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=fVF3J7KazZyQXX2R41ITkVIa6Y3UEd0o";
+ var queryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=fVF3J7KazZyQXX2R41ITkVIa6Y3UEd0o&limit=10`;
 
- fetch(queryURL)
- // then do something with the response (Promise)
-   .then((response) => response.json()) 
-   .then((myJson) => console.log(JSON.stringify(myJson)));
+ fetch(queryURL, {
+  method: "GET"
+})
+  // After data comes back from the request
+  .then(function(response) { return response.json() })
+  .then(function(response) {
+    console.log(queryURL);
 
-// variables
-// Initial array of animals
-var animals = ["dog", "cat", "mouse"];
+    console.log(response);
+    // storing the data from the AJAX request in the results variable
+    var results = response.data;
 
-// Function for displaying animals data
-var renderButtons = () => {
 
-    // Deleting the animals buttons prior to adding new animals buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    document.querySelector("#buttons-view").innerHTML = "";
+ // Looping through each result item
+ for (let item of results) {
 
- // Looping through the array of animals
- for (let animal of animals) {
+  // Creating and storing a div tag
+  var animalDiv = document.createElement("div");
 
-    // Then dynamicaly generating buttons for each animals in the array.
-    var a = document.createElement("button");
-    // Adding a class
-    a.classList.add("animal");
-    // Adding a data-attribute with a value of the animals at index i
-    a.setAttribute("data-name", animal);
-    // Providing the button's text with a value of the animals at index i
-    a.innerText = animal;
-    // Adding the button to the HTML
-    document.querySelector("#buttons-view").appendChild(a);
-  }
+  // Creating a paragraph tag with the result item's rating
+  var p = document.createElement("p")
+  p.innerText = `Rating: ${item.rating}`;
+
+  // Creating and storing an image tag
+  var animalImage = document.createElement("img");
+  // Setting the src attribute of the image to a property pulled off the result item
+  animalImage.setAttribute("src", item.images.fixed_height.url);
+
+  // Appending the paragraph and image tag to the animalDiv
+  animalDiv.appendChild(p);
+  animalDiv.appendChild(animalImage);
+
+  // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+  let gifContainer = document.querySelector("#gifs-appear-here");
+  gifContainer.prepend(animalDiv);
 }
- // This function handles events where one button is clicked
- document.querySelector("#add-animal").addEventListener("click", (event) => {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
-
-    // This line will grab the text from the input box
-    var animal = document.querySelector("#animal-input").value.trim();
-    // The animal from the textbox is then added to our array
-    animals.push(animal);
-
-    // calling renderButtons which handles the processing of our animal array
-    renderButtons();
-  });
+});
+}
+});
